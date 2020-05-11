3420f04cc635bd5cab80664635098a070893543c
ronpichardo <63974878+ronpichardo@users.noreply.github.com>
2020-05-11 14:10:50 -0400

    Added a If/Else and a While loop with minor desc

--- git.py
+++ git.py
@@ -1,3 +1,20 @@
 print('My first git file')
 
 print("my second change to this file")
+
+# Just assigning variable name to equal myName
+name = 'myName'
+
+# Because myName is true, it will print whatever is under the if block
+if name == 'myName':
+  print('Statement for name is true')
+# if you change name to equal something other than myName, the else block will print
+else:
+  print('name not found')
+  
+i = 0
+while(i <5):
+  print('Current i is: ' + i)
+  # Then we add 1 to i, so that it can reach 5 to end the loop
+  # i += 1 is the same as doing i = i + 1
+  i += 1
