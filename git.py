2eb0d2c1503da727552e26cc113f07c62f04e57c
ronpichardo <63974878+ronpichardo@users.noreply.github.com>
2020-05-11 14:43:49 -0400

    Changed name Variable to take in a user input

--- git.py
+++ git.py
@@ -3,14 +3,14 @@ print('My first git file')
 print("my second change to this file")
 
 # Just assigning variable name to equal myName
-name = 'myName'
+name = input("Input a name(Default, if you type in myName it will print correct statement): ")
 
 # Because myName is true, it will print whatever is under the if block
 if name == 'myName':
   print('Statement for name is true')
 # if you change name to equal something other than myName, the else block will print
 else:
-  print('name not found')
+  print(f'{name} not found')
   
 i = 0
 while(i <5):
